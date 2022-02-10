84418f6bd5348c2db801b8a0d4fa152f08b70d28
vasil tatarov <vasiltatarov3@gmail.com>
2022-02-10 20:08:57 +0200

    add superHeroApi project

---
+++ SuperHeroApi/SuperHeroApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,53 @@
+﻿namespace SuperHeroApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SuperHeroApi.Models;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>
+        {
+            new SuperHero
+            {
+                Id = 1,
+                Name = "Vasko",
+                Place = "Chakalo"
+            },
+            new SuperHero
+            {
+                Id = 2,
+                Name = "Sali",
+                Place = "Chakalo"
+            },
+        };
+
+        [HttpGet(Name = "GetSuperHeroes")]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetById(int id)
+        {
+            var hero = heroes.FirstOrDefault(x => x.Id == id);
+
+            if (hero == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(hero);
+        }
+
+        [HttpPost(Name = "AddHero")]
+        public async Task<ActionResult<int>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
+
+            return Ok(hero.Id);
+        }
+    }
+}
