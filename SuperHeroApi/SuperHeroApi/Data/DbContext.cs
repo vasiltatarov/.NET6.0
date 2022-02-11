0e520c8b8d404cd868b75d674f025f74b571a604
vasil tatarov <vasiltatarov3@gmail.com>
2022-02-11 04:47:35 +0200

    add sql database

---
+++ SuperHeroApi/SuperHeroApi/Data/DbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace SuperHeroApi.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using SuperHeroApi.Models;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
--- SuperHeroApi/SuperHeroApi/Program.cs
+++ SuperHeroApi/SuperHeroApi/Program.cs
@@ -1,13 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHeroApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var dbContext = services.GetRequiredService<DataContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
