ec18142360c086a3c5e1a86a5ccfab26d0639e06
vasil tatarov <vasiltatarov3@gmail.com>
2022-02-11 04:31:36 +0200

    implement CRUD operations

--- SuperHeroApi/SuperHeroApi/Controllers/SuperHeroController.cs
+++ SuperHeroApi/SuperHeroApi/Controllers/SuperHeroController.cs
@@ -36,7 +36,7 @@
 
             if (hero == null)
             {
-                return NotFound();
+                return BadRequest("Hero not found.");
             }
 
             return Ok(hero);
@@ -48,6 +48,39 @@
             heroes.Add(hero);
 
             return Ok(hero.Id);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<SuperHero>> UpdateHero(SuperHero request)
+        {
+            var hero = heroes.FirstOrDefault(x => x.Id == request.Id);
+
+            if (hero == null)
+            {
+                return BadRequest("Hero not found.");
+            }
+
+            hero.Name = request.Name;
+            hero.Place = request.Place;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+
+            return Ok(hero);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var hero = heroes.FirstOrDefault(x => x.Id == id);
+
+            if (hero == null)
+            {
+                return BadRequest("Hero not found.");
+            }
+
+            heroes.Remove(hero);
+
+            return Ok(heroes);
         }
     }
 }
