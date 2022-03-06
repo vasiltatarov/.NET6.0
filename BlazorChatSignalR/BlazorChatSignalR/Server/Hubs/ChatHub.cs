bf57f0404cffcfa783e5d985baa91e7633bbeae6
vasil tatarov <vasiltatarov3@gmail.com>
2022-03-06 15:52:25 +0200

    Blazor chat using SignalR

---
+++ BlazorChatSignalR/BlazorChatSignalR/Server/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorChatSignalR.Server.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+    using Microsoft.AspNetCore.SignalR.Client;
+
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await SendMessage("", "User connected!");
+
+            await base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("GetThatMessageDude", user, message);
+        }
+    }
+}
