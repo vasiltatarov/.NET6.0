a9eff7f4c3b8199565b41245fd12a05eb9764f6c
vasil tatarov <vasiltatarov3@gmail.com>
2022-04-17 16:29:30 +0300

    implemented hubConnection

--- BlazorChatSignalR/BlazorChatSignalR/Server/Hubs/ChatHub.cs
+++ BlazorChatSignalR/BlazorChatSignalR/Server/Hubs/ChatHub.cs
@@ -7,12 +7,12 @@
     {
         public override async Task OnConnectedAsync()
         {
-            await SendMessage("", "User connected!");
+            await AddMessageToChat("", "User connected!");
 
             await base.OnConnectedAsync();
         }
 
-        public async Task SendMessage(string user, string message)
+        public async Task AddMessageToChat(string user, string message)
         {
             await Clients.All.SendAsync("GetThatMessageDude", user, message);
         }
